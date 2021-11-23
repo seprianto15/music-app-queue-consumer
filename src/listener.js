bb50a429d1a8cd1ea9192cd4102dbfa8d1edff0b
Seprianto Sianipar <raysianipar15@gmail.com>
2021-11-23 15:14:31 +0700

    update consumer

--- src/listener.js
+++ src/listener.js
@@ -10,7 +10,7 @@ class Listener {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
 
-      const playlist = await this._playlistService.getPlaylistSong(playlistId);
+      const playlist = await this._playlistService.getPlaylists(playlistId);
       const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
       console.log(result);
     } catch (error) {
